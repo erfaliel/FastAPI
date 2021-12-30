d18e121c6e6fd746b76a445c7504f613e83c85ec
erfalielGarcia <fulielas@gmail.com>
2021-12-30 20:16:45 +0100

    add some methods and query parametes

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
+from typing import Optional
 from fastapi import FastAPI
-
 app = FastAPI()
 
-@app.get('/')        #add decocator from fastapi app objectg
-def index():
-    return 'heyy'
+#   '/blog?limit=10&published=true'        #we want handle parameters into the query
+@app.get("/blog")                          # parameters are handle with funtion parameter
+def index(limit: Optional[int] = 10, published:bool = True, sort: Optional[str] = None):
+    if published:
+        return {'data': 
+                    f'{limit} published blogs from the db'
+        }
+    else:
+        return {'data': 
+                    f'{limit} blogs from the db'
+        }
+
+@app.get('/blog/unpublished')
+def unpublished():
+    # fetch all unpublished blog
+    return {'data': 'All unpublished blogs'}
+    
+@app.get('/blog/{id}')
+def show(id: int):       #parameter form decorator need to be pass to the function
+    # fetch blog with id = id
+    return {'data': id}
+
+@app.get('/blog/{id}/comments')
+def comments(id: int):
+    # fetch comments of blog with id = id
+    return {'data': {id, "my comments"}}
