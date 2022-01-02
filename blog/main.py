9a0a1a52940784122897dd34e1db23ca09fe197a
erfalielGarcia <fulielas@gmail.com>
2022-01-02 21:57:31 +0100

    build db model

--- blog/main.py
+++ blog/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
-from . import schemas
+from sqlalchemy.sql.functions import mode
+from . import schemas, models
+from .database import engine
 
 app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
 
 @app.post("/blog")
 def create(blog: schemas.Blog):
     return blog
-
- 
 
