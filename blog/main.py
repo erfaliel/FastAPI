f5faf707b2d4193c3d4a261332cab39cf229124a
erfalielGarcia <fulielas@gmail.com>
2022-01-16 21:39:23 +0100

    hanndle HTTPException

--- blog/main.py
+++ blog/main.py
@@ -1,5 +1,4 @@
-from fastapi import FastAPI
-from fastapi.params import Depends
+from fastapi import FastAPI, Depends, status, HTTPException
 from . import schemas, models
 from .database import SessionLocal, engine
 from sqlalchemy.orm import Session
@@ -14,11 +13,25 @@ def get_db():
         yield db
     finally:
         db.close()
-
-@app.post("/blog")
+@app.post("/blog", status_code= status.HTTP_201_CREATED)
 def create(blog: schemas.Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title= blog.title, body= blog.body)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
     return new_blog
+
+@app.get("/blog")
+def all(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+@app.get("/blog/{id}", status_code= status.HTTP_200_OK)
+def show(id, db: Session = Depends(get_db)):
+    blog =  db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(
+            status_code= status.HTTP_404_NOT_FOUND,
+            detail=      f"Blog with the id {id} is not available")
+    return blog
+
